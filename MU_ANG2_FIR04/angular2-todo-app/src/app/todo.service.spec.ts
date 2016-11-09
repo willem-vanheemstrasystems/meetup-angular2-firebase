9126b0cfe4793911a7c0d6ea6c4f0a64419726e9
wvanheemstra <willem@vanheemstrasystems.com>
2016-11-09 16:52:52 +0100

    Modified

--- MU_ANG2_FIR04/angular2-todo-app/src/app/todo.service.spec.ts
+++ MU_ANG2_FIR04/angular2-todo-app/src/app/todo.service.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Todo } from './todo';
 import { TodoService } from './todo.service';
 
 describe('Service: Todo', () => {
@@ -9,8 +10,88 @@ describe('Service: Todo', () => {
       providers: [TodoService]
     });
   });
+  
+  describe('#getAllTodos()', () => {  
+  
+    it('should return an empty array by default', inject([TodoService], (service: TodoService) => {
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+    
+    it('should return all todos', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));      
+  
+  });
+  
+  describe('#save(todo)', () => {  
+  
+    it('should automatically assign an incrementing id', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getTodoById(1)).toEqual(todo1);
+      expect(service.getTodoById(2)).toEqual(todo2);
+    }));
+      
+  });  
+  
+  describe('#deleteTodoById(id)', () => {
 
-  it('should ...', inject([TodoService], (service: TodoService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should remove todo with the corresponding id', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(1);
+      expect(service.getAllTodos()).toEqual([todo2]);
+      service.deleteTodoById(2);
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+      
+    it('should not removing anything if todo with corresponding id is not found', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(3);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));    
+      
+  });    
+  
+  describe('#updateTodoById(id, values)', () => {  
+  
+    it('should return todo with the corresponding id and updated data', inject([TodoService], (service: TodoService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(1, {
+        title: 'new title'
+      });
+      expect(updatedTodo.title).toEqual('new title');
+    }));
+    
+    it('should return null if todo is not found', inject([TodoService], (service: TodoService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(2, {
+        title: 'new title'
+      });
+      expect(updatedTodo).toEqual(null);
+    }));    
+      
+  });   
+  
+  
+  
+  
+  
+  
+  
 });
