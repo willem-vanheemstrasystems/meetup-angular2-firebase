59d7a7ef1746d27fa1de5678ce9e305a4fa1acb9
wvanheemstra <willem@vanheemstrasystems.com>
2017-03-19 00:17:24 +0100

    Add ReactiveFormsModule

--- MU_ANG2_FIR21/apps/ourwebsite1_com-www/client2/src/app/app.module.ts
+++ MU_ANG2_FIR21/apps/ourwebsite1_com-www/client2/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from '@angular/material';
@@ -68,6 +68,7 @@ import { QuestionFormComponent } from './questions/shared/question-form/question
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,    
     AppRoutingModule,
     MaterialModule,
     AgmCoreModule.forRoot({
