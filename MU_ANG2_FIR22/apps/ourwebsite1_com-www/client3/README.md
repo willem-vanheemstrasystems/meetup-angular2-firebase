b0618e94784b490afea76d67c72d73bdc7984847
wvanheemstra <willem@vanheemstrasystems.com>
2017-04-10 23:14:52 +0200

    Add client3

---
+++ MU_ANG2_FIR22/apps/ourwebsite1_com-www/client3/README.md
@@ -0,0 +1,32 @@
+### README.md
+
+Includes webpack and scss to css
+
+Based on 'Webpack-101-starter' at https://github.com/Ihatetomatoes/webpack-101-starter
+
+See video at https://www.youtube.com/watch?v=JdGnYNtuEtE&list=PLkEZWD8wbltnRp6nRR8kv97RbpcUdNawY
+
+Run as follows for ***development***:
+
+```javascript
+npm run dev
+```
+
+Run as follows for ***production*** (will minify):
+
+```javascript
+npm run prod
+```
+
+Or better still, IN A SEPARATE TERMINAL, run as follows with the webpack dev server serving the index page from the dist folder:
+
+```javascript
+npm run build
+```
+
+Then browse for the application at http://localhost:8081 (port is set in package.json)
+
+DELETE - Image conversion based on https://github.com/tcoopman/image-webpack-loader
+
+Image conversion based on https://github.com/mschwarzmueller/yt-webpack2-basics
+
---
+++ MU_ANG2_FIR22/apps/ourwebsite1_com-www/client3/webpack.config.js
@@ -0,0 +1,90 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+    entry: './src/app/app.js',
+    output: {
+        path: path.resolve(__dirname, "../public/omnifood"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap'],
+                    publicPath: '../public/omnifood'
+                })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/,
+                loader: 'file-loader',
+                query: {
+                    name: '/assets/images/[name].[ext]'
+                }
+            }
+            //,
+            // {
+            //     test: /\.(gif|png|jpe?g|svg)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 name: '[name].[ext]',
+            //                 outputPath: 'images/',
+            //                 publicPath: 'images/'
+            //             }
+            //         }
+            //     ]
+            // }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "../public/omnifood"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Welcome | Omnifood',
+            lang: 'en',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: false,
+            allChunks: true
+        }),
+        new CleanWebpackPlugin(['../public/omnifood']),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/images',
+                to: 'assets/images'
+            },
+            {
+                from: 'src/assets/fonts',
+                to: 'assets/fonts'
+            }
+        ])
+    ]
+}
